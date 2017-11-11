c0b204eaf81353594c23b650e4020b041eee2faa
Maksim Tseljabov <Maksim.Tseljabov@itcollege.ee>
2017-11-11 23:14:17 +0200

    upd

--- mtseljab.py
+++ mtseljab.py
@@ -6,6 +6,12 @@ Skript loeb esimese parameetrina antud failist sisse URL-id (sisend.txt) ja nend
 sõned. Sõne ja URL eraldatakse, seejärel tehakse päring vastava URLi pihta. Vastuse lähtekoodist otsitakse URLi järel
 olnud sõnet. Teise parameetrina antud faili kirjutatakse uuesti URL, otsitav string ning "JAH/EI" vastavalt sellele,
 kas otsitav sõne leiti või mitte.
+--------------------------------------------------------------
+Usage:
+Install python 3.5 on your machine.
+Run script with SUDO privileges as follows:
+./mtseljab.py [INPUT FILE] [OUTPUT FILE]
+
 """
 
 import sys
@@ -14,23 +20,24 @@ from urllib.request import urlopen
 
 
 # http://pythoncentral.io/reading-and-writing-to-files-in-python/
-def clear_out_file():
+# Open output file (if doesn't exist it will be created) and remove all contents.
+def clear_out_file(file):
+    print("Creating/clearing output file {0}".format(file))
     try:
-        print("Clearing output file {0}".format(output_file_arg))
         file = open(output_file_arg, "w")
         file.truncate()
         file.close()
-        print("Success")
+        print("Success.")
     except OSError:
         print("It was not possible to open/create output file. Check your user privileges.")
         exit(1)
 
 
-# Open file, read lines, split it by comma, and save pairs to list
-def parse_input_file_to_list():
+# Read lines, split it by comma, and save pairs to list.
+def parse_input_file_to_list(file):
+    print("Opening input file {0}".format(file))
     try:
-        print("Opening input file {0}".format(input_file_arg))
-        input_file_object = open(input_file_arg, "r")
+        input_file_object = open(file, "r")
         print("Success")
     except FileNotFoundError as e:
         print("I/O error({0}): {1}".format(e.errno, e.strerror))
@@ -41,29 +48,15 @@ def parse_input_file_to_list():
     return input_lines_list
 
 
-def writefile(string):
+# Open and append output file
+def writefile(out_file, string):
     try:
-        file = open(output_file_arg, "a")
+        file = open(out_file, "a")
         file.write(string)
         file.close()
     except OSError as error:
         print("I/O error({0}): {1}".format(error.errno, error.strerror))
         exit(1)
-
-
-def open_read_url(URL):
-    try:
-        print("Opening URL {0}".format(URL))
-        response = urlopen(URL)
-        # print(response.info())
-        html = response.read()
-        response.close()
-        print("Success")
-        return html
-    except urllib.error.HTTPError as error:
-        print("HTTP Connection error ({0}): {1}: {2}".format(error.reason, error.reason, error.headers))
-    except urllib.error.URLError as error:
-        print("Oops. URL {0} error occured: {1}".format(item[0], error.reason))
 
 
 # Add "http://" if it's missing, then try to open URL. Handle HTTP and URL errors.
@@ -75,6 +68,23 @@ def create_correct_URL(input_link):
     return URL
 
 
+# Open URL and try to read html
+def open_read_url(URL):
+    try:
+        print("Opening URL {0}".format(URL))
+        response = urlopen(URL)
+        # print(response.info())
+        html = response.read()
+        response.close()
+        print("Success")
+        return html
+    except urllib.error.HTTPError as error:
+        print("HTTP Connection error ({0}): {1}: {2}".format(URL, error.reason, error.headers))
+    except urllib.error.URLError as error:
+        print("Oops. URL {0} error occured: {1}".format(item[0], error.reason))
+
+
+# Search word in html
 def search_html(html, word):
     result = ""
     string = str(html)
@@ -97,12 +107,12 @@ output_file_arg = "OUT.txt"
 input_lines_list = []
 
 # Start run with clearing OUT file
-clear_out_file()
+clear_out_file(output_file_arg)
 
 # Try to parse input file
-input_lines_list = parse_input_file_to_list()
+input_lines_list = parse_input_file_to_list(input_file_arg)
 
-# Check if data is present and search in html
+# Check if html data is present and search in html
 for item in input_lines_list:
     if len(item) == 2:
         URL = create_correct_URL(item[0])
@@ -110,15 +120,15 @@ for item in input_lines_list:
         if HTML == None:
             print("URL {0} is incorrect or cannot be reached. Skipping to next URL.".format(URL))
             out_line = "{0}: {1}: {2}\n".format(item[0], item[1], "URL cannot be reached")
-            writefile(out_line)
+            writefile(output_file_arg, out_line)
         else:
             result = search_html(HTML, item[1])
             out_line = "{0}: {1}: {2}\n".format(item[0], item[1], result)
-            writefile(out_line)
+            writefile(output_file_arg, out_line)
     else:
         result = "NO"
         out_line = "%s: %s \n" % (item[0], "Missing information in input file")
-        writefile(out_line)
+        writefile(output_file_arg, out_line)
 
 print("\nScript finished run successfully")
 
